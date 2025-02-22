f887323aed4115385a76a8a9739bc44e58abc37b
Pethum229 <pathumpp99@gmail.com>
2025-02-22 20:24:42 +0530

    first commit

---
+++ src/lib/api.js
@@ -0,0 +1,55 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+// Define a service using a base URL and expected endpoints
+export const Api = createApi({
+  reducerPath: "Api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${BASE_URL}/api/`,
+    prepareHeaders: async (headers, { getState }) => {
+      const token = await window.Clerk?.session?.getToken();
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => `products`,
+    }),
+    getCategories: builder.query({
+      query: () => `categories`,
+    }),
+    getOrder: builder.query({
+      query: (id) => `orders/${id}`,
+    }),
+    createOrder: builder.mutation({
+      query: (body) => ({
+        url: `orders`,
+        method: "POST",
+        body,
+      }),
+    }),
+    createCheckoutSession: builder.mutation({
+      query: () => ({
+        url: `/payment/create-checkout-session`,
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const {
+  useGetProductsQuery,
+  useGetCategoriesQuery,
+  useCreateOrderMutation,
+  useGetOrderQuery,
+  useCreateCheckoutSessionMutation,
+} = Api;
---
+++ src/pages/payment.page.jsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { clearCart } from "@/lib/features/cartSlice";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { toast } from "sonner";
+import CartItem from "@/components/CartItem";
+import { Navigate } from "react-router";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
+
+function PaymentPage() {
+  const cart = useSelector((state) => state.cart.value);
+  const dispatch = useDispatch();
+
+  if (cart.length === 0) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <main className="px-8">
+      <h2 className="text-4xl font-bold">Review Your Order</h2>
+      <div className="mt-4 grid grid-cols-4 gap-x-4">
+        {cart.map((item, index) => (
+          <CartItem key={index} item={item} />
+        ))}
+      </div>
+      <div className="mt-4">
+        <p>
+          Total Price: $
+          {cart.reduce(
+            (acc, item) => acc + item.product.price * item.quantity,
+            0
+          )}
+        </p>
+      </div>
+
+      <div className="mt-4">
+        {/* <Button
+          onClick={() => {
+            dispatch(clearCart());
+            toast.success("Order Placed Successfully");
+          }}
+        >
+          Place Order
+        </Button> */}
+        <CheckoutForm />
+      </div>
+    </main>
+  );
+}
+
+export default PaymentPage;
